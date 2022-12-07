303cba0ef2badeb1aad493158dd3097e3b45aa55
Amin Kamal <aminkamal355@gmail.com>
2022-12-07 21:26:07 +0800

    tugas 15
    pengumpulan tugas

---
+++ tugas15.js
@@ -0,0 +1,60 @@
+// // //pow
+
+
+// var x = 10;
+// console.log(Math.pow(x,2))
+
+
+// // // SQRT
+
+// function Akar(){
+//     console.log(Math.sqrt(25))
+// }
+
+// Akar();
+
+
+console.log("Sebuah segitiga siku-siku dengan Panjang Setiap sisi")
+console.log("AB = 8")
+console.log("AC= 6")
+console.log("AC = ?")
+console.log("Maka Berapakah Panjang AC?")
+
+
+var AB = 8;
+var BC = 6;
+
+
+console.log(Math.pow(AB,2));
+console.log(Math.pow(BC,2));
+console.log("------------------------------------------------------------------------------------------------")
+console.log("Nilai AB : 8")
+console.log("Nilai BC : 6")
+console.log("Maka Nilai AC Adalah AB Kuadrat + BC Kuadrat")
+console.log("8 Kuadrat = 64")
+console.log("6 Kuadrat = 36")
+console.log ("64 + 36" )
+console.log("AC =",100)
+console.log("------------------------------------------------------------------------------------------------")
+
+function Akar(){
+    console.log(Math.sqrt(100))
+}
+
+console.log("Akar dari 100 =")
+Akar();
+
+console.log("------------------------------------------------------------------------------------------------")
+console.log("Maka Panjang Semua Sisi Tersebut Adalah =")
+console.log("AB = 8")
+console.log("BC = 6")
+console.log("AC = 10")
+console.log("Terimakasih...")
+
+
+
+
+
+
+
+
